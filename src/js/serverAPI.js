48874876a38b087cfb34a5f26637015f54f26b7c
litichevskiy <kunin25@ukr.net>
2020-05-23 20:12:18 +0300

    added CORS proxy

--- src/js/serverAPI.js
+++ src/js/serverAPI.js
@@ -1,11 +1,12 @@
 import {fetch as fetchPolyfill} from 'whatwg-fetch';
 
 const API_KEY = 'b158c3465f5c44118acfd389f25d7549';
+const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
 const serverAPI = {
   async getNews( url ) {
     if( !url ) throw new Error(`url can't be ${url}`);
-    let response = await fetch( `${url}&apiKey=${API_KEY}` );
+    let response = await fetch( `${CORS_PROXY}${url}&apiKey=${API_KEY}` );
     if( response.status !== 200 ) {
       response = await response.json();
       throw new Error( response.message );
